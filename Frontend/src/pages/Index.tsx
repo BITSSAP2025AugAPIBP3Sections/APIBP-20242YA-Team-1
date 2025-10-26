e5d849d8479444440d90d900b9b7447936052c3a
Gourav garg <garggourav012@gmail.com>
2025-10-26 20:00:36 +0530

    Initialized project structure using vite

---
+++ Frontend/src/pages/Index.tsx
@@ -0,0 +1,16 @@
+import { Header } from "@/components/landing/Header";
+import { Hero } from "@/components/landing/Hero";
+import { Problem } from "@/components/landing/Problem";
+import { Features } from "@/components/landing/Features";
+import { Testimonials } from "@/components/landing/Testimonials";
+import { Footer } from "@/components/landing/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      Welcome!!
+    </div>
+  );
+};
+
+export default Index;
