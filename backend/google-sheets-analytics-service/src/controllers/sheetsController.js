ec0dcbba3df1778b1bf53852167c37362379919f
shreyanshig <cherrygolhani@gmail.com>
2025-10-29 21:53:12 +0530

    [googleSheetsAnalyticsSevice]: adding updatesummary and getsummary endpoints in the controller

---
+++ backend/google-sheets-analytics-service/src/controllers/sheetsController.js
@@ -0,0 +1,19 @@
+import { appendInvoiceData, fetchSummary, fetchTrends, exportSheetData } from "../services/sheetsService.js";
+
+export const updateSheet = async (req, res) => {
+  try {
+    const result = await appendInvoiceData(req.body);
+    res.json({ message: "Data added successfully", result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSummary = async (req, res) => {
+  try {
+    const data = await fetchSummary();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
